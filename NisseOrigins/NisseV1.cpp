86bc8abec1559f671579d5d7467be3ebbbd6e9d7
Loki Astari <Loki.Astari@gmail.com>
2024-11-05 10:47:33 -0800

    Add the basic flow.
        Added classes and tried to make the flow obvious.

--- NisseOrigins/NisseV1.cpp
+++ NisseOrigins/NisseV1.cpp
@@ -1,23 +1,52 @@
 #include <iostream>
 #include <exception>
 
+/*
+ * Class Declarations:
+ *
+ *      HttpRequest:        An HTTP request object that has been read from a 'Socket'.
+ *      HttpResponse:       An HTTP response object that can be written to a 'Socket' in
+ *                          response to an HttpRequest.
+ *      Socket:             A Unix socket connection that has been established.
+ *                          It acts like a bi-directional communication channel.
+ *                          It has an internal buffer to track requests.
+ *      Server:             A Unix socket listening for incoming connections.
+ *      WebServer:          A class to represent and manage incoming connections.
+ */
+class Socket;
+class HttpRequest
+{
+    public:
+        HttpRequest(Socket& socket);
+
+        bool valid() const;
+};
+
+class HttpResponse
+{
+    public:
+        HttpResponse(HttpRequest const& request);
+
+        void send(Socket& socket);
+};
+
 class Socket
 {
     public:
 };
 
-class ServerConnection
+class Server
 {
     public:
-        ServerConnection(int port);
+        Server(int port);
 
         Socket accept();
 };
 
 class WebServer
 {
-    ServerConnection    connection;
-    bool                finished;
+    Server      connection;
+    bool        finished;
     public:
         WebServer(int port);
 
@@ -26,6 +55,7 @@ class WebServer
         void handleConnection(Socket& socket);
 };
 
+// The application body.
 int main()
 {
     static constexpr int port = 8080;
@@ -47,9 +77,15 @@ int main()
     }
 }
 
+/*
+ * Class Implementation:
+ */
 
+// WebServer
+// =========
 WebServer::WebServer(int port)
-    : connection(port)
+    : connection{port}
+    , finished{false}
 {}
 
 void WebServer::run()
@@ -63,13 +99,42 @@ void WebServer::run()
 }
 
 void WebServer::handleConnection(Socket& socket)
+{
+    HttpRequest request(socket);
+
+    if (request.valid())
+    {
+        HttpResponse    response(request);
+        response.send(socket);
+    }
+}
+
+// Server
+// ======
+Server::Server(int port)
 {}
 
-ServerConnection::ServerConnection(int port)
-{}
-
-Socket ServerConnection::accept()
+Socket Server::accept()
 {
     return Socket{};
 }
+
+// HttpRequest
+// ===========
+HttpRequest::HttpRequest(Socket& socket)
+{}
+
+
+bool HttpRequest::valid() const
+{
+    return true;
+}
+
+// HttpResponse
+// ============
+HttpResponse::HttpResponse(HttpRequest const& request)
+{}
+
+void HttpResponse::send(Socket& socket)
+{}
 
