26acbdba79cee55a3abc12c3c5a0ed6a5886acca
Loki Astari <Loki.Astari@gmail.com>
2024-11-05 10:30:49 -0800

    Add the basic server style in place

--- NisseOrigins/NisseV1.cpp
+++ NisseOrigins/NisseV1.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 #include <exception>
 
+class WebServer
+{
+    public:
+        WebServer(int port);
+
+        void run();
+};
+
 int main()
 {
+    static constexpr int port = 8080;
     try
     {
         std::cout << "Nisse Proto 1\n";
+        WebServer   server(port);
+        server.run();
     }
     catch(std::exception const& e)
     {
@@ -18,3 +29,10 @@ int main()
         throw;
     }
 }
+
+
+WebServer::WebServer(int port)
+{}
+
+void WebServer::run()
+{}
