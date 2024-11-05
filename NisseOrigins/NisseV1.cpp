82e572a639d2958c3215d71dec17be9d1133fefa
Loki Astari <Loki.Astari@gmail.com>
2024-11-05 10:27:35 -0800

    Add exception messaging

--- NisseOrigins/NisseV1.cpp
+++ NisseOrigins/NisseV1.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <exception>
 
 int main()
 {
-    std::cout << "Nisse Proto 1\n";
+    try
+    {
+        std::cout << "Nisse Proto 1\n";
+    }
+    catch(std::exception const& e)
+    {
+        std::cerr << "Exception: " << e.what() << "\n";
+        throw;
+    }
+    catch(...)
+    {
+        std::cerr << "Exception: UNKNOWN\n";
+        throw;
+    }
 }
